b096001efdd16040b2cb15e58b636a317fdb9d07
SopnilSwapon <sopnilswapon1@gmail.com>
2025-11-28 09:05:42 +0600

    force rebuild

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,15 +24,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
-
-  app.use(express.static(frontendPath));
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
-}
-
 server.listen(port, () => {
   console.log(`Server is running @${port} port`);
   connectDB();
