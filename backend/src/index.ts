cc78227a0675b57c1e78ea192ad7a569ea9e05b0
SopnilSwapon <sopnilswapon1@gmail.com>
2025-11-28 08:42:24 +0600

    updated the production logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,3 @@
-// import express from "express";
-// import authRoutes from "./routes/authRoute";
-// import messageRoutes from "./routes/messageRoute";
-// import dotenv from "dotenv";
-// import { connectDB } from "./lib/db";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import { app, server } from "./lib/socket";
-// import path from "path";
-
-// dotenv.config();
-
-// const port = process.env.PORT || 3001;
-// const __dirname = path.resolve();
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
-// server.listen(port, () => {
-//   console.log(`Server is running @${port} port`);
-//   connectDB();
-// });
-
 import express from "express";
 import authRoutes from "./routes/authRoute";
 import messageRoutes from "./routes/messageRoute";
@@ -63,10 +25,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "../../frontend/dist");
 
+  app.use(express.static(frontendPath));
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
